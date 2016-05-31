e87c40811b295fc5c4f46fe029dc0b09366634bb
Tomoya Chiba <tomo.asleep@gmail.com>
2016-05-31 23:12:01 +0900

    Update download_dotfiles

--- scripts/download_dotfiles.sh
+++ scripts/download_dotfiles.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-# TODO: Write download script
+check_git () {
+  if which git; then; return; fi
+  if which brew; then; brew install git; return; fi
+  if which apt; then; sudo apt install -y git; return; fi
+  if which pacman; then; sudo pacman -S git; return; fi
+}
+
+check_git
+git clone git@github.com:tomoasleep/dotfiles.git ~/dotfiles
+~/dotfiles/install.sh
